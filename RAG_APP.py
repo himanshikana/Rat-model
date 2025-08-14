8d2af542f799c88b91e02ea293f76a6d0c3d26d6
himanshi shah <146656806+himanshikana@users.noreply.github.com>
2025-08-14 12:52:40 +0530

    Update RAG_APP.py

--- RAG_APP.py
+++ RAG_APP.py
@@ -23,7 +23,7 @@ if not groq_api_key:
 
 st.set_page_config(page_title="Dynamic RAG with Groq", layout="wide")
 
-st.image("AA.jpg")
+st.image("images.jpg")
 
 st.title("Dynamic RAG with Groq, FAISS, and Llama3")
 
